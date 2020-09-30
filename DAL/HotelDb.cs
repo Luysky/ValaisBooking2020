ce842a6caac0730aeebe57077413e420d40e40fa
mathieu <mathieu.kohl@students.hevs.ch>
2020-09-30 13:26:53 +0200

    GetAllHotels

---
+++ DAL/HotelDb.cs
@@ -0,0 +1,63 @@
+﻿using DTO;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class HotelDb : IHotelDb
+    {
+        public IConfiguration Configuration { get; }
+
+        public HotelDb(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        //list of every Hotels from every city
+        public List<Hotel> GetAllHotels(int id)
+        {
+            List<Hotel> results = null;
+            string ConnectionStrings = Configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(ConnectionStrings)) 
+                {
+                    string query = "SELECT * FROM Hotel";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cn.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            if (results == null)
+                                results = new List<Hotel>();
+
+                            Hotel hotel = new Hotel();
+
+                            hotel.IdHotel = (int)dr["IdHotel"];
+                            hotel.Name = (string)dr["Name"];
+                            hotel.Description = (string)dr["Description"];
+                            hotel.Location = (string)dr["Location"];
+                            hotel.Category = (int)dr["Category"];
+                            hotel.HasWifi = (bool)dr["HasWifi"];
+                            hotel.HasParking = (bool)dr["HasParking"];
+                            hotel.Email = (string)dr["Email"];
+                            hotel.Website = (string)dr["Website"];
+                            results.Add(hotel);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return results;
+        }
+    }
+}
---
+++ DAL/IHotelDb.cs
@@ -0,0 +1,15 @@
+﻿using DTO;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public interface IHotelDb 
+    {
+        IConfiguration Configuration { get; }
+        List<Hotel> GetAllHotels(int id);
+       
+    }
+}
