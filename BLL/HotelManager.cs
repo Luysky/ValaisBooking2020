cdca2be8ccd2ae7529ede74f69c69319c7c435ef
mathieu <mathieu.kohl@students.hevs.ch>
2020-09-30 13:33:02 +0200

    BLL GetAllHotels

---
+++ BLL/HotelManager.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using DTO;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL
+{
+    public class HotelManager : IHotelManager
+    {
+        public IHotelDb HotelDb { get; }
+
+        public HotelManager(IConfiguration configuration)
+        {
+            HotelDb = new HotelDb(configuration);
+        }
+
+        public List<Hotel> GetAllHotels(int id)
+        {
+            return HotelDb.GetAllHotels(id);
+        }
+    }
+}
---
+++ BLL/IHotelManager.cs
@@ -0,0 +1,15 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public interface IHotelManager
+    {
+        IHotelDb HotelDb { get; }
+
+        List<Hotel> GetALlHotels(int id);
+
+    }
+}
